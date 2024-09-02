d5c00edee9f7d2689b528a3da779baa56fab62d5
masicheng <297876926@qq.com>
2024-09-02 15:18:00 +0800

    chore(api:upload): upload新增刷新token的逻辑

--- src/api/sys/upload.ts
+++ src/api/sys/upload.ts
@@ -4,9 +4,12 @@ import { UploadFileParams } from '#/axios';
 import { useGlobSetting } from '@/hooks/setting';
 import { AxiosProgressEvent } from 'axios';
 import { useMessage } from '@/hooks/web/useMessage';
+import { AxiosRefreshToken } from '@/utils/http/axios/axiosRefreshToken';
+import { ResultEnum } from '@/enums/httpEnum';
 
 const { uploadUrl = '' } = useGlobSetting();
 const { createMessage } = useMessage();
+const axiosRefreshToken = new AxiosRefreshToken();
 /**
  * @description: Upload interface
  */
@@ -26,6 +29,8 @@ export function uploadApi(
       .then((res) => {
         if (res.data.code === 200) {
           resolve(res.data);
+        } else if (res.data.code === ResultEnum.TIMEOUT) {
+          return axiosRefreshToken.refeshing(res);
         } else {
           createMessage.error(res.data.msg);
           reject(res.data);
